f92fff0e84bd30c84302fb9aa5edd3bf0bd05ec5
Patrik Hedman <patrik@moresale.se>
2009-08-16 14:53:49 +0200

    got the tests passing

--- spec/video_advanced_options_spec.rb
+++ spec/video_advanced_options_spec.rb
@@ -11,7 +11,7 @@ describe "FFMpeg Video Advanced Options" do
       pixel_format 'list'
     end
     
-    FFMpegCommand.command("ffmpeg").should match("-pix_fmt list")
+    FFMpegCommand.command("ffmpeg").should match(/-pix_fmt list/)
   end
 
   it "should set the SwScaler flags" do
@@ -19,7 +19,7 @@ describe "FFMpeg Video Advanced Options" do
       swscaler_flags 'flag'
     end
     
-    FFMpegCommand.command("ffmpeg").should match("-sws_flags flag")
+    FFMpegCommand.command("ffmpeg").should match(/-sws_flags flag/)
   end
 
   it "should set the group of pictures size" do
@@ -27,7 +27,7 @@ describe "FFMpeg Video Advanced Options" do
       group_of_pictures_size 200
     end
     
-    FFMpegCommand.command("ffmpeg").should match("-g 200")
+    FFMpegCommand.command("ffmpeg").should match(/-g 200/)
   end
 
   it "should set to use only intra frames" do
@@ -35,7 +35,7 @@ describe "FFMpeg Video Advanced Options" do
       use_only_intra_frames
     end
     
-    FFMpegCommand.command("ffmpeg").should match("-intra")
+    FFMpegCommand.command("ffmpeg").should match(/-intra/)
   end
 
   it "should set the discard threshold" do
@@ -43,7 +43,7 @@ describe "FFMpeg Video Advanced Options" do
       video_discard_threshold 20
     end
     
-    FFMpegCommand.command("ffmpeg").should match("-vdt 20")
+    FFMpegCommand.command("ffmpeg").should match(/-vdt 20/)
   end
 
   it "should set to use a fixed video quantizer scale" do
@@ -51,7 +51,7 @@ describe "FFMpeg Video Advanced Options" do
       video_quantizer_scale 10
     end
     
-    FFMpegCommand.command("ffmpeg").should match("-qscale 10")
+    FFMpegCommand.command("ffmpeg").should match(/-qscale 10/)
   end
 
   it "should set the minimum video quantizer scale" do
@@ -59,7 +59,7 @@ describe "FFMpeg Video Advanced Options" do
       video_minimum_quantizer_scale 10
     end
     
-    FFMpegCommand.command("ffmpeg").should match("-qmin 10")
+    FFMpegCommand.command("ffmpeg").should match(/-qmin 10/)
   end
 
   it "should set the maximum video quantizer scale" do
@@ -67,7 +67,7 @@ describe "FFMpeg Video Advanced Options" do
       video_maximum_quantizer_scale 10
     end
     
-    FFMpegCommand.command("ffmpeg").should match("-qmax 10")
+    FFMpegCommand.command("ffmpeg").should match(/-qmax 10/)
   end
   
   it "should set the maximum video quantizer difference" do
@@ -75,7 +75,7 @@ describe "FFMpeg Video Advanced Options" do
       video_maximum_quantizer_difference 10
     end
     
-    FFMpegCommand.command("ffmpeg").should match("-qdiff 10")
+    FFMpegCommand.command("ffmpeg").should match(/-qdiff 10/)
   end
 
   it "should set the video quantizer scale blur" do
@@ -83,7 +83,7 @@ describe "FFMpeg Video Advanced Options" do
       video_quantizer_scale_blur 0.6
     end
     
-    FFMpegCommand.command("ffmpeg").should match("-qblur 0.6")
+    FFMpegCommand.command("ffmpeg").should match(/-qblur 0.6/)
   end
 
   it "should set the video quantizer scale compression" do
@@ -91,7 +91,7 @@ describe "FFMpeg Video Advanced Options" do
       video_quantizer_scale_compression 0.7
     end
     
-    FFMpegCommand.command("ffmpeg").should match("-qcomp 0.7")
+    FFMpegCommand.command("ffmpeg").should match(/-qcomp 0.7/)
   end
 
   it "should set the minimum video lagrange factor" do
@@ -99,7 +99,7 @@ describe "FFMpeg Video Advanced Options" do
       video_minimum_lagrange_factor 10
     end
     
-    FFMpegCommand.command("ffmpeg").should match("-lmin 10")
+    FFMpegCommand.command("ffmpeg").should match(/-lmin 10/)
   end
 
   it "should set the maximum video lagrange factor" do
@@ -107,7 +107,7 @@ describe "FFMpeg Video Advanced Options" do
       video_maximum_lagrange_factor 10
     end
     
-    FFMpegCommand.command("ffmpeg").should match("-lmax 10")
+    FFMpegCommand.command("ffmpeg").should match(/-lmax 10/)
   end
 
   it "should set the minimum video macroblock quantizer scale" do
@@ -115,7 +115,7 @@ describe "FFMpeg Video Advanced Options" do
       video_minimum_macroblock_scale 10
     end
     
-    FFMpegCommand.command("ffmpeg").should match("-mblmin 10")
+    FFMpegCommand.command("ffmpeg").should match(/-mblmin 10/)
   end
 
   it "should set the maximum video macroblock quantizer scale" do
@@ -123,7 +123,7 @@ describe "FFMpeg Video Advanced Options" do
       video_maximum_macroblock_scale 10
     end
     
-    FFMpegCommand.command("ffmpeg").should match("-mblmax 10")
+    FFMpegCommand.command("ffmpeg").should match(/-mblmax 10/)
   end
 
   it "should set the initial complexity for single pass encoding" do
@@ -131,7 +131,7 @@ describe "FFMpeg Video Advanced Options" do
       initial_complexity 5
     end
     
-    FFMpegCommand.command("ffmpeg").should match("-rc_init_cplx 5")
+    FFMpegCommand.command("ffmpeg").should match(/-rc_init_cplx 5/)
   end
 
   it "should set the qp factor between P-frames and B-frames" do
@@ -139,7 +139,7 @@ describe "FFMpeg Video Advanced Options" do
       b_frame_factor 10
     end
     
-    FFMpegCommand.command("ffmpeg").should match("-b_qfactor 10")
+    FFMpegCommand.command("ffmpeg").should match(/-b_qfactor 10/)
   end
 
   it "should set the qp factor between P-frames and I-frames" do
@@ -147,7 +147,7 @@ describe "FFMpeg Video Advanced Options" do
       i_frame_factor 10
     end
     
-    FFMpegCommand.command("ffmpeg").should match("-i_qfactor 10")
+    FFMpegCommand.command("ffmpeg").should match(/-i_qfactor 10/)
   end
 
   it "should set the qp offset between P-frames and B-frames" do
@@ -155,7 +155,7 @@ describe "FFMpeg Video Advanced Options" do
       b_frame_offset 10
     end
     
-    FFMpegCommand.command("ffmpeg").should match("-b_qoffset 10")
+    FFMpegCommand.command("ffmpeg").should match(/-b_qoffset 10/)
   end
 
   it "should set the qp offset between P-frames and I-frames" do
@@ -163,7 +163,7 @@ describe "FFMpeg Video Advanced Options" do
       i_frame_offset 10
     end
     
-    FFMpegCommand.command("ffmpeg").should match("-i_qoffset 10")
+    FFMpegCommand.command("ffmpeg").should match(/-i_qoffset 10/)
   end
 
 
@@ -172,7 +172,7 @@ describe "FFMpeg Video Advanced Options" do
       rate_control_equation "tex^qComp"
     end
     
-    FFMpegCommand.command("ffmpeg").should match("-rc_eq string")
+    FFMpegCommand.command("ffmpeg").should match(/-rc_eq string/)
   end
 
   it "should set the rate control override" do
@@ -180,7 +180,7 @@ describe "FFMpeg Video Advanced Options" do
       rate_control_override 10
     end
     
-    FFMpegCommand.command("ffmpeg").should match("-rc_override 10")
+    FFMpegCommand.command("ffmpeg").should match(/-rc_override 10/)
   end
 
   it "should set the motion estimation method" do
@@ -188,7 +188,7 @@ describe "FFMpeg Video Advanced Options" do
       motion_estimation_method "hex"
     end
     
-    FFMpegCommand.command("ffmpeg").should match("-me_method hex")
+    FFMpegCommand.command("ffmpeg").should match(/-me_method hex/)
   end
 
   it "should set the DCT algorithm" do
@@ -196,7 +196,7 @@ describe "FFMpeg Video Advanced Options" do
       dct_algorithm 5
     end
     
-    FFMpegCommand.command("ffmpeg").should match("-dct_algo 5")
+    FFMpegCommand.command("ffmpeg").should match(/-dct_algo 5/)
   end
 
   it "should set the IDCT algorithm" do
@@ -204,7 +204,7 @@ describe "FFMpeg Video Advanced Options" do
       idct_algorithm 8
     end
     
-    FFMpegCommand.command("ffmpeg").should match("-idct_algo 8")
+    FFMpegCommand.command("ffmpeg").should match(/-idct_algo 8/)
   end
 
   it "should set error resilience" do
@@ -212,7 +212,7 @@ describe "FFMpeg Video Advanced Options" do
       error_resilience 2
     end
     
-    FFMpegCommand.command("ffmpeg").should match("-er 2")
+    FFMpegCommand.command("ffmpeg").should match(/-er 2/)
   end
 
   it "should set error concealment" do
@@ -220,7 +220,7 @@ describe "FFMpeg Video Advanced Options" do
       error_concealment 1
     end
     
-    FFMpegCommand.command("ffmpeg").should match("-ec 1")
+    FFMpegCommand.command("ffmpeg").should match(/-ec 1/)
   end
 
   it "should set to use B-frames" do
@@ -228,7 +228,7 @@ describe "FFMpeg Video Advanced Options" do
       b_frames 15
     end
     
-    FFMpegCommand.command("ffmpeg").should match("-bf 15")
+    FFMpegCommand.command("ffmpeg").should match(/-bf 15/)
   end
 
   it "should set macroblock decision mode" do
@@ -236,7 +236,7 @@ describe "FFMpeg Video Advanced Options" do
       macroblock_decision_mode 1
     end
     
-    FFMpegCommand.command("ffmpeg").should match("-mbd 1")
+    FFMpegCommand.command("ffmpeg").should match(/-mbd 1/)
   end
 
   it "should use four motion vector by macroblock" do
@@ -244,7 +244,7 @@ describe "FFMpeg Video Advanced Options" do
       use_four_motion_vector
     end
     
-    FFMpegCommand.command("ffmpeg").should match("-4mv")
+    FFMpegCommand.command("ffmpeg").should match(/-4mv/)
   end
 
   it "should set to use data partitioning" do
@@ -252,7 +252,7 @@ describe "FFMpeg Video Advanced Options" do
       use_data_partitioning
     end
     
-    FFMpegCommand.command("ffmpeg").should match("-part")
+    FFMpegCommand.command("ffmpeg").should match(/-part/)
   end
 
   # `-bug param'
@@ -263,7 +263,7 @@ describe "FFMpeg Video Advanced Options" do
       strictness 3
     end
     
-    FFMpegCommand.command("ffmpeg").should match("-strict 3")
+    FFMpegCommand.command("ffmpeg").should match(/-strict 3/)
   end
 
   it "should enable advanced intra coding" do
@@ -271,7 +271,7 @@ describe "FFMpeg Video Advanced Options" do
       enable_advanced_intra_coding
     end
     
-    FFMpegCommand.command("ffmpeg").should match("-aic")
+    FFMpegCommand.command("ffmpeg").should match(/-aic/)
   end
 
   it "should enable unlimited motion vector" do
@@ -279,7 +279,7 @@ describe "FFMpeg Video Advanced Options" do
       enable_unlimited_motion_vector
     end
     
-    FFMpegCommand.command("ffmpeg").should match("-umv")
+    FFMpegCommand.command("ffmpeg").should match(/-umv/)
   end
 
   it "should deinterlace pictures" do
@@ -287,7 +287,7 @@ describe "FFMpeg Video Advanced Options" do
       deinterlace
     end
     
-    FFMpegCommand.command("ffmpeg").should match("-deinterlace")
+    FFMpegCommand.command("ffmpeg").should match(/-deinterlace/)
   end
 
   it "should force interlacing support in encoder" do
@@ -295,7 +295,7 @@ describe "FFMpeg Video Advanced Options" do
       interlacing_support
     end
     
-    FFMpegCommand.command("ffmpeg").should match("-ilme")
+    FFMpegCommand.command("ffmpeg").should match(/-ilme/)
   end
 
   it "should calculate PSNR of compressed frames" do
@@ -303,7 +303,7 @@ describe "FFMpeg Video Advanced Options" do
       calculate_psnr
     end
     
-    FFMpegCommand.command("ffmpeg").should match("-psnr")
+    FFMpegCommand.command("ffmpeg").should match(/-psnr/)
   end
 
   it "should dump video coding statistic to log file" do
@@ -311,7 +311,7 @@ describe "FFMpeg Video Advanced Options" do
       dump_video_statistics
     end
     
-    FFMpegCommand.command("ffmpeg").should match("-vstats")
+    FFMpegCommand.command("ffmpeg").should match(/-vstats/)
   end
 
   it "should dump video statistics to named file" do
@@ -319,7 +319,7 @@ describe "FFMpeg Video Advanced Options" do
       dump_video_statistics "video_stats.log"
     end
     
-    FFMpegCommand.command("ffmpeg").should match("-vstats_file video_stats.log")
+    FFMpegCommand.command("ffmpeg").should match(/-vstats_file video_stats.log/)
   end
 
   # `-top n'
@@ -336,7 +336,7 @@ describe "FFMpeg Video Advanced Options" do
       show_qp_histogram
     end
     
-    FFMpegCommand.command("ffmpeg").should match("-qphist")
+    FFMpegCommand.command("ffmpeg").should match(/-qphist/)
   end
 
   it "should set the video bitstream filter" do
@@ -344,6 +344,6 @@ describe "FFMpeg Video Advanced Options" do
       video_bitstream_filter "dump_extra"
     end
     
-    FFMpegCommand.command("ffmpeg").should match("-vbsf dump_extra")
+    FFMpegCommand.command("ffmpeg").should match(/-vbsf dump_extra/)
   end
 end
