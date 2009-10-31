80db267b575ed9c3cd43ea562af75eef7464b04e
Patrik Hedman <patrik@moresale.se>
2009-10-31 13:55:40 +0100

    added lib to spec load path

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'spec'
 
 $LOAD_PATH.unshift(File.dirname(__FILE__))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
 require 'ffmpeg'
 include FFMpeg
